e6aaa9f6d05df61219464c44ade7dfccf2639f1e
Mateus Machado <mateus.machado@passeidireto.com>
2022-08-03 14:56:46 -0300

    feat: minor changes to aws and runner scripts

--- Linux/Utils/gh-get-pending-runner.sh
+++ Linux/Utils/gh-get-pending-runner.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 for row in $(gh repo list PasseiDireto -L 850 --json name -q '.[] | .name'); do
     gh run list -R PasseiDireto/${row} --json url,name,status,createdAt,startedAt,updatedAt -q '.[] | select(.status=="queued") | .url' >>pending_wfs.txt
 done
