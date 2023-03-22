59dbbb08f7967f6230f5f62cdc3388f15bcb090d
Mateus Machado <mateus.machado@passeidireto.com>
2023-03-22 10:57:11 -0300

    Update gh-clone.sh

--- Linux/Utils/gh-clone.sh
+++ Linux/Utils/gh-clone.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-RepoList=("athena-salesforce-integration-job" "data-airflow-stack" "data-jobs" "data-platform-adhoc-analysis" "gh-runner"
-    "gh-runner-stack" "gh-sysbox-image-builder-stack" "mrr-forecast-model-job" "onboarding-mateus" "pd-airflow-lib" "pdlytics-stack")
+RepoList=("athena-salesforce-integration-job" "code-metadata-update-tool" "data-airflow-dags" "data-airflow-stack" data-dms-stack "data-jobs" "data-platform-adhoc-analysis"
+    "data-lake-quality-check" "gh-runner" "gh-runner-check-stuck-stack" "gh-runner-stack" "gh-runner-task-action" "gh-sysbox-image-builder-stack" "mrr-forecast-model-job"
+    "pd-airflow-lib" "pd-backstage" "pd-backstage-templates" "pd-github-billing-job" "pdlytics-stack" "pd-roles-stack" "pd-sso")
 
-cd /home/matvieira/Documents/Git
+cd /home/pd-mateus/Documents/Git
 
 for repo in "${RepoList[@]}"; do
     echo "Cloning the repo $repo..."
